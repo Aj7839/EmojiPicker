f320ff4d1b76ccc496914ffe936cb153249f83a5
Aj <alexjacques04@Outlook.com>
2024-01-14 17:15:08 +0000

    Initial Commit

---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by Alex Wilson  on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
